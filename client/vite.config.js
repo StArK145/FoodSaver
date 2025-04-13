e07b3572ba0b4cfcb7512c2d2a76e7941657a66b
Your Name <sushilphadtare2005@gmail.com>
2025-04-13 15:55:11 +0530

    ready to deploy4

--- client/vite.config.js
+++ client/vite.config.js
@@ -20,5 +20,15 @@ export default defineConfig({
         '/api': 'http://localhost:5000', // assuming backend runs on port 5000
       },
     },
-    
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return id.toString().split('node_modules/')[1].split('/')[0].toString();
+            }
+          }
+        }
+      }
+    }
 })
